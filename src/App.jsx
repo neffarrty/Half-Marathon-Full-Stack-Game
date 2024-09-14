e92999b749bff73c7ca79d9a04f9f3433dc39d3d
neffarrty <egorkovtun8@gmail.com>
2024-09-14 23:20:22 +0300

    create basic structure of app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+      <Card name='Captain America' desc='Huilo'/>
+    </>
+  )
+}
+
+export default App
