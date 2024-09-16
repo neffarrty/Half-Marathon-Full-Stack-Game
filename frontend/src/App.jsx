1901cea2e70ee28c15760887a33dd4609915a805
neffarrty <egorkovtun8@gmail.com>
2024-09-16 18:34:39 +0300

    add basic routing and update login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import './styles/App.css'
-import Card from './components/Card'
+import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <>
-      <Card name='Captain America' desc='Huilo'/>
-    </>
-  )
-}
-
-export default App
+export default function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</Router>
+	);
+};
