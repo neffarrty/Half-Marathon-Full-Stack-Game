f0dfcd65d99f059685984d3634ba321805ff412a
Andrii Zhupanov <Andrii@MacBook-Pro-Andrey.local>
2024-09-17 12:32:43 +0200

    add homepage visual and search animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path='/' element={<Home />} />
+				<Route path='/home' element={<Home />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
 			</Routes>
